f6d15d6529733abf0c655f972be89ce5ce3f877a
Abdoulkarim <amahamn@gmail.com>
2023-01-05 23:25:17 +0100

    final

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,12 +4,6 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """  Sums a list 
-    Args:
-        input_list (list): A list of floats
-    Returns:
-        float: sum 
-    """
     if input_list is None:
         return 0
     else:
